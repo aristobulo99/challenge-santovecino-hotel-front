6a126ac7364f22002dd3d2ad51c3178399fc25d0
juniorespriella99 <juniorespriella99@gmail.com>
2024-11-28 16:36:47 -0500

    fix: creacion del componente y ruta disponibilidad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModel } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { AvailabilityComponent } from './pages/availability/availability.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'availability',
+        component: AvailabilityComponent
     },
     {
         path:'**',
