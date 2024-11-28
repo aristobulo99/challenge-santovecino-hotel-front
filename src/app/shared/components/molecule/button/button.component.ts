907ba1d63ccb46f63298ebd83892833046fa0c09
juniorespriella99 <juniorespriella99@gmail.com>
2024-11-28 13:34:04 -0500

    fix: creacion del componete button

---
+++ src/app/shared/components/molecule/button/button.component.ts
@@ -0,0 +1,29 @@
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [
+    NgClass
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+  @Input() name!: String; // nombre asignado al boton
+  @Input() size: 'large' | 'medium' | 'small' = 'large'; // control la altura del boton;
+  @Input() type: 'flat' | 'outline' = 'flat'; //Control el color del boton
+  @Input() valid: boolean = true; //Control si el boton esta activo
+
+  @Output() clickEvent: EventEmitter<void> = new EventEmitter();
+
+  clickButton(){
+    if(this.valid){
+      this.clickEvent.emit();
+    }
+  }
+
+
+}
