e7874a2ec08eccd9978d06cd984135fe9ac9b821
juniorespriella99 <juniorespriella99@gmail.com>
2024-11-28 20:39:03 -0500

    fix: maquetacion de card y ajustes de padding

--- src/app/pages/availability/availability.component.ts
+++ src/app/pages/availability/availability.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SimpleCardComponent } from '../../shared/components/atom/simple-card/simple-card.component';
 
 @Component({
   selector: 'app-availability',
   standalone: true,
-  imports: [],
+  imports: [SimpleCardComponent],
   templateUrl: './availability.component.html',
   styleUrl: './availability.component.scss'
 })
---
+++ src/app/shared/components/atom/simple-card/simple-card.component.ts
@@ -0,0 +1,19 @@
+import { NgClass, NgTemplateOutlet } from '@angular/common';
+import { Component, Input, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-card',
+  standalone: true,
+  imports: [
+    NgTemplateOutlet,
+    NgClass
+  ],
+  templateUrl: './simple-card.component.html',
+  styleUrl: './simple-card.component.scss'
+})
+export class SimpleCardComponent {
+
+  @Input() type: 'simple' | 'turquoise' = 'simple';
+  @Input() content!: TemplateRef<any>;
+
+}
